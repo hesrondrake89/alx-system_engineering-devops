07371cf6ad49c915b291228da3266ecde8cc7419
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-04-26 14:41:24 +0000

    tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-# Creates a file in /tmp
+# Create a file in /tmp
 
 file { 'school':
-`:x
-:x
-
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744', 
+  path    => '/tmp/school',
+}
